034b649545d47be129e2e1f4b5f0b8e926aea76c
TahaJahani <taha7900@gmail.com>
2022-02-15 07:47:07 +0330

    lostAndFound controller and router added

---
+++ src/controllers/lostAndFoundController.js
@@ -0,0 +1,19 @@
+module.exports = {
+    getAll: (req, res, next) => {
+
+    },
+
+    add: (req, res, next) => {
+        
+    },
+
+    remove: (req, res, next) => {
+        
+    },
+
+    return: (req, res, next) => {
+        
+    },
+
+    
+}
---
+++ src/routes/lostAndFoundRouter.js
@@ -0,0 +1,13 @@
+const withAuthentication = require('../middlewares/withAuthentication')
+const withAbility = require('../middlewares/withAbility')
+const lostAndFoundController = require('../controllers/lostAndFoundController')
+var router = require('express').Router();
+router.use(withAuthentication)
+
+
+router.get('/', lostAndFoundController.getAll)
+router.post('/', withAbility(['owner', 'admin', 'financial']), lostAndFoundController.add)
+router.delete('/:found_id', withAbility(['owner', 'admin', 'financial']), lostAndFoundController.delete)
+router.post('/:found_id', withAbility(['owner', 'admin', 'financial']), lostAndFoundController.return)
+
+module.exports = router;
