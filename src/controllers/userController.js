5e334386f295ddf99000cebd713349d484538679
TahaJahani <taha7900@gmail.com>
2022-02-15 12:01:48 +0330

    userController functions implemented

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,23 +4,65 @@ const tokenGenerator = require('../services/token')
 const bcrypt = require('bcryptjs');
 module.exports = {
     completeUserInfo: async (req, res, next) => {
-        
+        let err = validator.check(req.body, {
+            name: 'required|regex:/^[ آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی]+$/',
+            surname: 'required|regex:/^[ آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی]+$/',
+            phone_number: 'numeric',
+        })
+        if (err.length > 0)
+            return res.json({status: 'error', message: err[0]})
+        if (req.user || req.surname)
+            return res.json({status: 'error', message: 'ویرایش دوباره اطلاعات امکان پذیر نیست'})
+        let to_update = {
+            name: req.body.name,
+            surname: req.body.surname,
+        }
+        if (req.body.phone_number)
+            to_update.phone_number = req.body.phone_number
+        let user = (await User.update(to_update, {where: {id: req.user.id}}))[1]
+        return res.json({status: 'ok', data: {user: user}})
     },
 
     checkLogin: async (req, res, next) => {
-
+        if (!req.body.token)
+            return res.json({status: 'error', message: 'unauthorized'})
+        let token_id = req.body.token.split('|')[0]
+        let token_body = req.body.token.split('|')[1]
+        let tokenRecord = PersonalAccessToken.findOne({where: {id: token_id}})
+        if (tokenRecord && bcrypt.compareSync(token_body, tokenRecord.token))
+            return res.json({status: 'ok'})
+        return res.json({status: 'error', message: 'unauthorized'})
     },
 
     getUser: async (req, res, next) => {
-
+        let err = validator.check(req.params, {
+            student_number: 'required|numeric'
+        })
+        if (err.length > 0)
+            return res.json({status: 'error', message: err[0]})
+        let user = await User.findOne({where: {student_number: req.params.student_number}})
+        let status = user ? 'ok' : 'error'
+        return res.json({status: status, data: {user: user}})
     },
 
     getUsers: async (req, res, next) => {
-
+        let offset = ((req.query.page ? req.query.page : 1) - 1) * 50;
+        let users = await User.findAndCountAll({
+            include: ['roles'],
+            offset: offset,
+            limit: 15,
+        })
+        let hasNext = users.count > offset + 15;
+        let lastPage = Math.ceil(users.count / 15);
+        users = users.rows
+        return res.json({status: 'ok', data: {data: users, has_next: hasNext, last_page: lastPage}})
     },
 
     getAllUsersStudentNumber: async (req, res, next) => {
-
+        let users = User.findAll({
+            attributes: ['id', 'student_number']
+        })
+        return res.json({status: 'ok', data: {users: users}})
     },
 
     login: async (req, res, next) => {
@@ -59,7 +101,7 @@ module.exports = {
     },
 
     changePassword: async (req, res, next) => {
-
+        
     },
 
     logout: async (req, res, next) => {
